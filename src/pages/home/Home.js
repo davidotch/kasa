c4756631b3beb2fa4aef74c3098c17e1a76b8656
David Cardon <da.cardon@outlook.fr>
2023-04-04 23:58:25 +0200

    import Gallery.js to Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import Header from '../../components/header/Header'
 import Banner from '../../components/banner/Banner'
+import Gallery from '../../components/gallery/Gallery'
 
 export function Home() {
     return (
         <div className='home'>
             <Header/>
             <Banner/>
+            <Gallery/>
         </div>
     )
 }
