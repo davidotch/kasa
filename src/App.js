27f1257a4d0acf976e20137fa4f87c365437a125
David Cardon <da.cardon@outlook.fr>
2023-04-04 23:39:04 +0200

    create routes App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/home/Home';
+import Accomodation from './pages/accomodation/Accomodation';
+import About from './pages/about/About';
+import PageNotFound from './pages/pageNotFound/PageNotFound';
 
 const router = createBrowserRouter([
-
-
+  {
+    path: "/kasa/",
+    element: <Home/>
+  },
+  {
+    path: "/Accomodation/",
+    element: <Accomodation/>
+  },
+  {
+    path: "/About/",
+    element: <About/>
+  },
+  {
+    path: "*",
+    element: <PageNotFound/>
+  }
 ]);
 
 function App() {
