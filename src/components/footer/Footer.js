6f34f4f3db919a8821326134d0a7b0ec0339bc81
David Cardon <da.cardon@outlook.fr>
2023-04-11 10:29:23 +0200

    create footer folder & Footer.js file

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    )
+        <div className="footer">
+      <div className="foot_Logo">
+        <img
+          src={require("../assets/logoBlk.png")}
+          alt="Logo de l'agence Kasa avec le premier A en forme de maison"
+        />
+      </div>
+      <div className="foot_copyright">
+        <p>&#169; Kasa. All rights reserved</p>
+      </div>
+    </div>
+    );
 } 
