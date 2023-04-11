503b21ff9be92c11e66c9d7a427fb5e428e2fc28
David Cardon <da.cardon@outlook.fr>
2023-04-11 10:40:47 +0200

    create & import Footer.scss

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,15 @@
-import React from "react";
+import './Footer.scss'
 
 export default function Footer() {
     return (
         <div className="footer">
-      <div className="foot_Logo">
+      <div className="footer_Logo">
         <img
-          src={require("../assets/logoBlk.png")}
+          src={require("../../assets/logoBlk.png")}
           alt="Logo de l'agence Kasa avec le premier A en forme de maison"
         />
       </div>
-      <div className="foot_copyright">
+      <div className="footer_copyright">
         <p>&#169; Kasa. All rights reserved</p>
       </div>
     </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import Banner from "../../components/banner/Banner";
+import Footer from "../../components/footer/Footer";
 
 export default function Home() {
 	return (
 		
 		<div className="Home">
 			<Banner />
+			<Footer />
 		</div>
 	);
 }
