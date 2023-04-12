d1a3d0fdfbf187baefe978dc23a50e037763cdf0
David Cardon <da.cardon@outlook.fr>
2023-04-12 18:54:08 +0200

    add Collapse description & import about.json

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,3 +1,4 @@
+import about from '../../data/about.json';
 import Header from '../../components/header/Header';
 import Banner from '../../components/banner/Banner';
 import Collapse from '../../components/collapse/Collapse';
@@ -8,7 +9,10 @@ export default function About() {
         <div>
             <Header />
             <Banner />
-            <Collapse />
+            <Collapse title="Fiabilité" description={about.fiabilite} />
+            <Collapse title="Respect" description={about.respect} />
+            <Collapse title="Service" description={about.service} />
+            <Collapse title="Sécurité" description={about.securite} />
             <Footer />
         </div>
     )
