45c067e5cc1bdd67a82ee8bdbafc2f828a4c149a
David Cardon <da.cardon@outlook.fr>
2023-04-12 18:23:30 +0200

    create collapse.js & add arrow.png

---
+++ src/components/collapse/Collapse.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import arrow from '../../assets/arrow.png';
+
+export default function Collapse({ title, description }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="collapse">
+      <h3 className='collapse_title' onClick={handleClick}>
+                    {title}
+                    <img 
+                        className={open ? 'arrow arrow_up' : 'arrow arrow_down'} 
+                        src={arrow} 
+                        alt="show content" 
+                    />
+                </h3>
+      {open ? (
+        <div className="collapse_description">
+          <p>{description}</p>
+        </div>
+      ) : (
+        <div className="collapse_hidden_description">{description}</div>
+      )}
+    </div>
+  );
+}
