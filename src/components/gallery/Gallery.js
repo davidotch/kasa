73ab53e536b760cfa7d70c10904d2546b73accd1
David Cardon <da.cardon@outlook.fr>
2023-04-13 14:05:27 +0200

    create fetch Gallery.js

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,11 +1,36 @@
 import Card from '../card/Card'
-import datas from '../../data/data'
 import './Gallery.scss'
+import { useEffect, useState } from 'react';
 
 export default function Gallery() {
+
+    const [ data, setData ] = useState();
+
+    useEffect(() => {
+
+        const getData = async () => {
+            fetch('../data.json')
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error("Sorry an error occured...");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setData(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+        getData();
+    });
+    
+    
+
     return (
         <main className='home_accomodation'>
-            {datas.map(data => {
+            {data && data.map(data => {
                 return (
                     <Card
                         key={data.id}
