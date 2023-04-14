37bea9829b64ffd7d52a489f3eb50cb66e4c03da
David Cardon <da.cardon@outlook.fr>
2023-04-14 16:39:09 +0200

    add comment

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,4 +1,4 @@
-import './banner.scss'
+import './banner.scss';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -7,17 +7,21 @@ export default function Banner() {
 	const [aboutPage, setAboutPage] = useState(false);
 
 	const location = useLocation();
-	
+
+
+// le hook useEffect de React définit une variable d'état "aboutPage" à "true" si l'URL actuelle est "/about".
+// La variable d'état "aboutPage" est utilisée pour afficher un message différent sur la bannière en fonction de la page actuelle.   
+// Si la page n'est pas "/about", le message par défaut "Chez vous, partout et ailleurs" sera affiché. 
 	useEffect(() => {
 		if(location.pathname === '/about'){
-			setAboutPage(true)
+			setAboutPage(true);
 		};
 		// eslint-disable-next-line
-	}, [])
+	}, []);
 
 	return (
 		<section className={aboutPage ? 'banner_about' : 'banner'}>
-			{!aboutPage && <p>Chez vous, partout et ailleurs</p>}
+			{!aboutPage && <p>Chez vous, partout et ailleurs</p>};
 		</section>
-	)
-}
+	);
+};
