c83a7ca5be20c895414ca62052e3fac9b668336c
David Cardon <da.cardon@outlook.fr>
2023-04-14 16:53:26 +0200

    add comment

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -3,9 +3,13 @@ import { useState } from 'react';
 import arrowBack from '../../assets/carousel_arrow_back.png';
 import arrowForward from '../../assets/carousel_arrow_forward.png';
 
+// La fonction Carousel prend en paramètre un tableau d'images (imageSlider) et affiche une image à la fois, en fonction de l'indice (currentIndex).
 export default function Carousel({ imageSlider }) {
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  // Les fonctions nextImg() et previousImg() sont appelées lorsqu'on clique sur les flèches "next" et "back".
+  
+  // La fonction nextImg() incrémente l'indice.
   const nextImg = () => {
     if (currentIndex === imageSlider.length - 1) {
       setCurrentIndex(0);
@@ -14,6 +18,7 @@ export default function Carousel({ imageSlider }) {
     }
   };
 
+  //  la fonction previousImg() décrémente l'indice.
   const previousImg = () => {
     if (currentIndex === 0) {
       setCurrentIndex(imageSlider.length - 1);
@@ -22,6 +27,8 @@ export default function Carousel({ imageSlider }) {
     }
   };
 
+  //  L'image affichée est définie comme fond de la div, avec l'indice de l'image.
+  // Si le tableau contient plus d'une image, les flèches et l'indicateur de position/nombre total sont affichés.
   return (
     <div className="carousel" style={{backgroundImage: `url(${imageSlider[currentIndex]})`}}>
       {imageSlider.length > 1 && (
@@ -30,19 +37,21 @@ export default function Carousel({ imageSlider }) {
             className="carousel_arrow carousel_arrow_back"
             src={arrowBack}
             alt="Show previous img"
-            onClick={previousImg}
+            onClick={nextImg}
           />
           <img
             className="carousel_arrow carousel_arrow_forward"
             src={arrowForward}
             alt="Show next img"
-            onClick={nextImg}
+            onClick={previousImg}
           />
+
+          {/* le composant est exporté en tant que composant par défaut. */}
           <p className="slideCount">
             {currentIndex + 1}/{imageSlider.length}
           </p>
         </>
-      )}
+      )};
     </div>
   );
-}
+};
