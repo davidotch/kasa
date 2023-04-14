385b304613168f932b8a414a0dcb774021c2c559
David Cardon <da.cardon@outlook.fr>
2023-04-14 11:28:08 +0200

    fix function onclick

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -1,17 +1,14 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import arrow from '../../assets/arrow.png';
 import './Collapse.scss';
 
 export default function Collapse({ title, description }) {
-  const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="collapse">
-      <h3 className='collapse_title' onClick={handleClick}>
+      <h3 className='collapse_title' onClick={() => setOpen(!open)}>
             {title}
             <img 
                 className={open ? 'arrow arrow_up' : 'arrow arrow_down'} 
@@ -20,7 +17,7 @@ export default function Collapse({ title, description }) {
             />
       </h3>
 
-      {/* Le contenu de la DIV est déterminé par la variable content. Si content est un tableau, chaque élément est converti en un paragraphe HTML et ajouté à la DIV. Sinon, content est affiché tel quel dans la DIV. */}
+      {/* Le contenu de la DIV est déterminé par la variable 'description'. Si 'description' est un tableau, chaque élément est converti en un paragraphe HTML et ajouté à la DIV. Sinon, 'description' est affiché tel quel dans la DIV. */}
       <div className={open ? 'collapse_description' : 'collapse_hidden_description'}>
             {Array.isArray(description) ? description.map((item, index) => {
                 return (
