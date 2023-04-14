a06edc3db4a41690824dc52c83fe26414c504406
David Cardon <da.cardon@outlook.fr>
2023-04-14 10:12:38 +0200

    fix description Collapse.js

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -12,20 +12,24 @@ export default function Collapse({ title, description }) {
   return (
     <div className="collapse">
       <h3 className='collapse_title' onClick={handleClick}>
-                    {title}
-                    <img 
-                        className={open ? 'arrow arrow_up' : 'arrow arrow_down'} 
-                        src={arrow} 
-                        alt="show content" 
-                    />
-                </h3>
-      {open ? (
-        <div className="collapse_description">
-          <p>{description}</p>
-        </div>
-      ) : (
-        <div className="collapse_hidden_description">{description}</div>
-      )}
-    </div>
+            {title}
+            <img 
+                className={open ? 'arrow arrow_up' : 'arrow arrow_down'} 
+                src={arrow} 
+                alt="show content" 
+            />
+      </h3>
+
+      {/* Le contenu de la DIV est déterminé par la variable content. Si content est un tableau, chaque élément est converti en un paragraphe HTML et ajouté à la DIV. Sinon, content est affiché tel quel dans la DIV. */}
+      <div className={open ? 'collapse_description' : 'collapse_hidden_description'}>
+            {Array.isArray(description) ? description.map((item, index) => {
+                return (
+                    <p key={index}>{item}</p>
+                  )
+              }) : description
+            }
+      </div> 
+
+    </div>  
   );
 }
