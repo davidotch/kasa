5a02cb1cb3accf410f5ee8a70eb1f14c5b4b6ed8
David Cardon <da.cardon@outlook.fr>
2023-04-18 19:14:16 +0200

    fix description/add ul, li

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -19,13 +19,16 @@ export default function Collapse({ title, description }) {
 
       {/* Le contenu de la DIV est déterminé par la variable 'description'. Si 'description' est un tableau, chaque élément est converti en un paragraphe HTML et ajouté à la DIV. Sinon, 'description' est affiché tel quel dans la DIV. */}
       <div className={open ? 'collapse_description' : 'collapse_hidden_description'}>
-            {Array.isArray(description) ? description.map((item, index) => {
-                return (
-                    <p key={index}>{item}</p>
-                  )
-              }) : description
-            }
-      </div> 
+        {Array.isArray(description) ?
+          <ul>
+            {description.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+          : description
+        }
+      </div>
+
 
     </div>  
   );
