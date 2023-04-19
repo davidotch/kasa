9df609eb4df4bd22d503ab2ae76cda99749112e3
David Cardon <da.cardon@outlook.fr>
2023-04-20 01:57:44 +0200

    fix banner(props)

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,11 +1,10 @@
-import React from "react";
 import './banner.scss';
 
-export default function Banner ({ text, picture }) {
-	return (
-		<div className="banner">
-			<img className="banner_img" src={picture} alt="banner" />
-			<h1 className="banner_title">{text}</h1>
-		</div>
-	);
+export default function Banner(props) {
+    return (
+        <div className={props.class}>
+            <img src={props.image} alt={props.alt} />
+            {props.children}
+        </div>
+    );
 }
