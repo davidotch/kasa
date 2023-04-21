2d6fb337f642676f08dc288df9c362002c0bdf61
David Cardon <da.cardon@outlook.fr>
2023-04-21 13:35:27 +0200

    add comment

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,5 +1,6 @@
 import './banner.scss';
 
+// La fonction Banner prend un objet props en paramètre et utilise les propriétés de cet objet pour spécifier les attributs CSS de la bannière, son image et son texte
 export default function Banner(props) {
     return (
         <div className={props.class}>
