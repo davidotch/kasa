265768386bd8040430f77868cf9c78b5bfe8e4f3
David Cardon <da.cardon@outlook.fr>
2023-04-21 17:43:27 +0200

    add comment

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import PageNotFound from './pages/pageNotFound/PageNotFound';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 
+// La fonction 'App' retourne un élément qui est encapsulé dans 'BrowserRouter' pour gérer la navigation.
+// 'Routes' renvoie vers les différents élément en fonction de l'URL. 
+// La propriété 'element' contient l'élément react que l'itinéraire renvoie si le chemin d'accès correspond. 
+// Le symbole '*' signifie que ce chemin d'accès est associé à toutes les autres URL qui ne correspondent pas. 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/accomodation/:id" element={<Accomodation />} />
